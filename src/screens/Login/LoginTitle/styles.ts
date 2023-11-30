b488649d5d68f9b524b983fc8dcaf99ea4bc6214
VitaliyWebdev <mv703021@gmail.com>
2023-11-30 16:00:12 +0200

    Added Login title

---
+++ src/screens/Login/LoginTitle/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  titleWrapper: {
+    marginTop: 60,
+  },
+  title: {
+    color: "#000",
+    fontSize: 32,
+    fontWeight: "500",
+    lineHeight: 38,
+    letterSpacing: -0.408,
+    marginBottom: 12,
+  },
+  subtitle: {
+    color: "rgba(0, 0, 0, 0.50)", // Use the desired color
+    fontSize: 15,
+    fontWeight: "400",
+    lineHeight: 18,
+    letterSpacing: -0.408,
+  },
+});
+
+export default styles;
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,11 +1,13 @@
 import SafeViewWrapper from "@src/components/SafeViewWrapper";
 import { FC } from "react";
 import LoginHeader from "./LoginHeader";
+import LoginTitle from "./LoginTitle";
 
 const Login: FC = () => {
   return (
     <SafeViewWrapper>
       <LoginHeader />
+      <LoginTitle />
     </SafeViewWrapper>
   );
 };
