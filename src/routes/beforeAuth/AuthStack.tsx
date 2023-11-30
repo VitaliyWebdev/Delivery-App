f9a411869c4841e52cc520c9adcc9aa90267c09f
VitaliyWebdev <mv703021@gmail.com>
2023-11-30 15:48:23 +0200

    screenOptions extracted to separate object

--- src/routes/beforeAuth/AuthStack.tsx
+++ src/routes/beforeAuth/AuthStack.tsx
@@ -5,9 +5,16 @@ import { publicRoutes } from "@src/constants/routes";
 
 const Stack = createStackNavigator();
 
+const screenOptions = {
+  headerShown: false,
+};
+
 const AuthStack = () => {
   return (
-    <Stack.Navigator initialRouteName={publicRoutes.Login}>
+    <Stack.Navigator
+      initialRouteName={publicRoutes.Login}
+      screenOptions={screenOptions}
+    >
       <Stack.Screen name={publicRoutes.Login} component={Login} />
       <Stack.Screen name={publicRoutes.SignUp} component={SignUp} />
     </Stack.Navigator>
